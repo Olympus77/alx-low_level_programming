09a16b61ef2f27959f6b18e3962cf478c48f6f79
Olympus77 <ndunguolympus@gmail.com>
2023-09-11 14:50:42 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - print the lowercase alphabet in revese
+ * Return: Always (success)
+ */
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+		putchar(d);
+	putchar("\n")
+		return (0)
+}
